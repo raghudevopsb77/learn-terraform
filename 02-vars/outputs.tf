26cfdc10de25ccd44514c6a5d3d0f9ab4ac42361
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-16 08:28:16 +0530

    Ansible Code

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -2,3 +2,15 @@ output "v1" {
   value = var.v1
 }
 
+output "First_Value_in_List" {
+  value = var.v2[0]
+}
+
+output "Second_Value_in_List" {
+  value = var.v2[1]
+}
+
+output "Value_in_Map" {
+  value = var.v3["abc"]
+}
+
--- 02-vars/variables.tf
+++ 02-vars/variables.tf
@@ -1,3 +1,16 @@
 variable "v1" {
   default = "Hello World"
 }
+
+variable "v2" {
+  default = [ "Hello World", 100, true ]
+}
+## A variable list can have different data types
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "Two Hundred"
+  }
+}
+
