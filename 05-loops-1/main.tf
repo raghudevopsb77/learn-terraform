7c54734317152d7f41f2c784ebeb1d9a1a1f74b5
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-17 08:09:42 +0530

    Ansible Code

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -2,6 +2,8 @@ resource "null_resource" "test" {
   count = 10
 }
 
+# By providing count , resource will become list
+
 output "test" {
-  value = null_resource.test[0].id
+  value = null_resource.test.*.id
 }
