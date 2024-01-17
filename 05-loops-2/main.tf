8461823930d6ef9338a50566fabb092a803597e9
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-17 08:31:53 +0530

    Ansible Code

---
+++ 05-loops-2/main.tf
@@ -0,0 +1,27 @@
+variable "fruits1" {
+  default = {
+    apple = 10
+    banana = 100
+  }
+}
+
+
+variable "fruits2" {
+  default = {
+    apple = {
+      name = "apple"
+      quantity = 10
+    }
+    banana = {
+      name = "banana"
+      quantity = 100
+    }
+  }
+}
+
+
+resource "dummy_thing" "this1" {
+  for_each = var.fruits1
+  name = "${each.key} = ${each.value}"
+}
+
