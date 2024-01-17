7a951f5bc87574d6446fb73421c38c2b577f3560
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-17 07:33:59 +0530

    Ansible Code

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami                     = "ami-03265a0778a880afb"
+  ami                     = "ami-0f3c7d07486cad139"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = ["sg-04ce0678d07a32073"]
 
@@ -9,7 +9,7 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_instance" "backend" {
-  ami                     = "ami-03265a0778a880afb"
+  ami                     = "ami-0f3c7d07486cad139"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = ["sg-04ce0678d07a32073"]
 
@@ -19,7 +19,7 @@ resource "aws_instance" "backend" {
 }
 
 resource "aws_instance" "mysql" {
-  ami                     = "ami-03265a0778a880afb"
+  ami                     = "ami-0f3c7d07486cad139"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = ["sg-04ce0678d07a32073"]
 
