b6b3197d3695ca5f5a64cfcd818ccd77b291114b
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-17 07:57:50 +0530

    Ansible Code

---
+++ expense-v2/variables.tf
@@ -0,0 +1,12 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-04ce0678d07a32073"]
+}
+
+variable "zone_id" {
+  default = "Z09059901XRPHNYMGLMJ4"
+}
+
