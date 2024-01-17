199fad7ecb491ace581a56859c1975f2fe5136f0
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-17 08:16:30 +0530

    Ansible Code

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -8,14 +8,29 @@ output "test" {
   value = null_resource.test.*.id
 }
 
+#resource "aws_instance" "test" {
+#  count                   = 3
+#  ami                     = "ami-0f3c7d07486cad139"
+#  instance_type           = "t3.micro"
+#  vpc_security_group_ids  =  ["sg-04ce0678d07a32073"]
+#
+#  tags = {
+#    Name = "test-${count.index}"
+#  }
+#}
+
+variable "components" {
+  default = [ "frontend", "backend", "mysql"]
+}
+
 resource "aws_instance" "test" {
-  count                   = 3
+  count                   = length(var.components)
   ami                     = "ami-0f3c7d07486cad139"
   instance_type           = "t3.micro"
   vpc_security_group_ids  =  ["sg-04ce0678d07a32073"]
 
   tags = {
-    Name = "test-${count.index}"
+    Name = element(var.components, count.index)
   }
 }
 
