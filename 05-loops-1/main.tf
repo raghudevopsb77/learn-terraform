88b3310cfda1466abf7ed64a9c8890cb28ce20b4
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-17 08:11:38 +0530

    Ansible Code

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -7,3 +7,15 @@ resource "null_resource" "test" {
 output "test" {
   value = null_resource.test.*.id
 }
+
+resource "aws_instance" "test" {
+  count                   = 3
+  ami                     = "ami-0f3c7d07486cad139"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  =  ["sg-04ce0678d07a32073"]
+
+  tags = {
+    Name = "test"
+  }
+}
+
